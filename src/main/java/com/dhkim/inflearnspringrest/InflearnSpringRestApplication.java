9e194e0f7dbb17b9bbf87d2ea979f073ac1afe87
DaeHo Kim <rlaeogh222@gmail.com>
2018-12-11 16:26:38 +0900

    initialize project

---
+++ src/main/java/com/dhkim/inflearnspringrest/InflearnSpringRestApplication.java
@@ -0,0 +1,12 @@
+package com.dhkim.inflearnspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringRestApplication.class, args);
+	}
+}
