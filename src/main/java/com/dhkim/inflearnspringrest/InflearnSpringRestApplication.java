c6701d656a71e4978533214aaac36eb4556564e4
DaeHo Kim <rlaeogh222@gmail.com>
2018-12-11 16:57:10 +0900

    create project

--- src/main/java/com/dhkim/inflearnspringrest/InflearnSpringRestApplication.java
+++ src/main/java/com/dhkim/inflearnspringrest/InflearnSpringRestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InflearnSpringRestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(InflearnSpringRestApplication.class, args);
 	}
 }
